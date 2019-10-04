fcd8e3c2aaf69fc466fe90d3131a3855455a5e1a
paxarpp <pavelpp1974@gmail.com>
2019-10-04 20:39:52 +0300

    add flat style button

--- stories/button.svelte
+++ stories/button.svelte
@@ -3,15 +3,57 @@
 </button>
 
 <style>
-    .button {
-      border: 1px solid #eee;
-      border-radius: 3px;
-      backgroundColor: #FFFFFF;
-      cursor: pointer;
-      font-size: 15px;
-      padding: 3px 10px;
-      margin: 10px;
+    @keyframes ripple {
+      0% {
+        background-color: #2bbbad;
+        transform: scale(0, 0);
+        opacity: 0.5;
+      }
+      20% {
+        transform: scale(25, 25);
+        opacity: 0.5;
+      }
+      100% {
+        background-color: #2bbbad;
+        opacity: 0;
+        transform: scale(40, 40);
+      }
     }
+    .button {
+      position: relative;
+      overflow: hidden;
+      border: none;
+      border-radius: 2px;
+      display: inline-block;
+      height: 36px;
+      line-height: 36px;
+      padding: 0 16px;
+      font-size: 14px;
+      text-transform: uppercase;
+      vertical-align: middle;
+      color: #343434;
+      background-color: transparent;
+      text-align: center;
+      letter-spacing: 0.5px;
+      cursor: pointer;
+      outline: none;
+    }
+    .button:after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 5px;
+      height: 5px;
+      background: rgba(0, 0, 0, 0.5);
+      opacity: 0;
+      border-radius: 100%;
+      transform: scale(1, 1) translate(-50%);
+      transform-origin: 50% 50%;
+    }
+    .button:focus:not(:active):after {
+    animation: ripple 1s ease-out;
+  }
 </style>
 
 <script>
@@ -21,7 +63,5 @@
 
   const dispatch = createEventDispatcher();
 
-  function onClick(event) {
-    dispatch('click', event)
-  }
+  const onClick = (event) => dispatch('click', event)
 </script>
